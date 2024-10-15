71c706ca1a80f2a0dddba588341778a6768ca02c
Abel Sánchez <abelsanchez@solemti.com>
2024-10-14 22:26:50 -0700

    has been added car service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,26 +1,21 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['toyota', 'mercedes'];
+  constructor(public readonly carsService: CarsService) {}
+
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
-  @Get(':id') //?para poder hacer la peticion get y que pueda recibir parametros
+  @Get(':id')
+  //?para poder hacer la peticion get y que pueda recibir parametros
   getCarById(@Param('id') id) {
     //?EL @Param se utiliza para poder decorar la variable id y que sea como param.
-    const number = Number(id);
-    if (!number) {
-      return {
-        message: 'this is not a number',
-      };
-    }
-    const car = this.cars[+id];
-    if (!car) return { message: 'No car id' };
-    return {
-      car: this.cars[+id],
-    };
+    const idn = Number(id);
+    if (!idn) return { message: 'id not valid' };
+    return this.carsService.getCarById(+id);
   }
 }
--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class CarsService {}
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'toyoya',
+      price: '100000',
+    },
+    {
+      id: 2,
+      brand: 'nissan',
+      price: '1000',
+    },
+    {
+      id: 3,
+      brand: 'bmw',
+      price: '1000100',
+    },
+    {
+      id: 4,
+      brand: 'mercedes',
+      price: '100000010',
+    },
+  ];
+
+  public findAll() {
+    return this.cars;
+  }
+
+  public getCarById(id: number) {
+    const car = this.cars.find((auto) => auto.id === id);
+    if (!car) return { message: 'car not found' };
+    return car;
+  }
+}
